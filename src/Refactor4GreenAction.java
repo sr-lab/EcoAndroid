71b4669e0bd2b848587b8eaae14ab0e764427b97
anasofiagribeiro <asofiagribeiro@gmail.com>
2020-03-04 20:29:17 +0000

    first commit

---
+++ src/Refactor4GreenAction.java
@@ -0,0 +1,25 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiIdentifier;
+import org.jetbrains.annotations.NotNull;
+
+public class Refactor4GreenAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+        Editor editor = anActionEvent.getData(CommonDataKeys.EDITOR);
+        PsiFile psiFile = anActionEvent.getData(CommonDataKeys.PSI_FILE);
+        if (editor == null || psiFile == null) return;
+        int offset = editor.getCaretModel().getOffset();
+
+        final StringBuilder infoBuilder = new StringBuilder();
+        PsiElement element = psiFile.findElementAt(offset);
+        infoBuilder.append("Element at caret: ").append(element).append("\n\n");
+        Messages.showMessageDialog(anActionEvent.getProject(), infoBuilder.toString(), "PSI Info", null);
+
+    }
+}
