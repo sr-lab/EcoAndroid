d58c28c530c6c687d040fe478ff7eae16532245a
anasofiagribeiro <asofiagribeiro@gmail.com>
2020-04-08 18:11:03 +0100

    checkNetwork update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Refactor4Green
-An Android Studio Plugin for Reducing Energy Consumption of Java Mobile Applications through Automated Refactoring
+An Android Studio Plugin for Reducing Energy Consumption of Java Mobile Applications through Automated Refactoring.
+<br/> 
+The energy patterns the plugin can detect are: <br/>
+  1. *Dynamic Retry Delay*, with the following cases:<br/>
+   * Switching the time a thread goes to sleep from constant to dynamic (only considering the cases where variables are used to put the thread to sleep)<br/>
+   * Checking the network connection before processing a thread <br/>
--- src/main/java/DynamicRetryDelay/CheckNetwork/CheckNetworkQuickFix.java
+++ src/main/java/DynamicRetryDelay/CheckNetwork/CheckNetworkQuickFix.java
@@ -4,6 +4,9 @@ import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.psi.codeStyle.JavaCodeStyleManager;
+import com.intellij.psi.search.GlobalSearchScope;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 public class CheckNetworkQuickFix implements LocalQuickFix {
@@ -34,13 +37,28 @@ public class CheckNetworkQuickFix implements LocalQuickFix {
     public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor problemDescriptor) {
 
         PsiElementFactory factory = JavaPsiFacade.getInstance(project).getElementFactory();
+        PsiIdentifier psiIdentifier = (PsiIdentifier) problemDescriptor.getPsiElement();
+        PsiMethod psiMethod = (PsiMethod) psiIdentifier.getContext();
+        PsiClass intentServiceClass = psiMethod.getContainingClass();
+
+        //TODO: ir buscar a variavel de classe que implemente SharedPreferences.onSharedPreferenceChangeListener
 
         /*
         *
         * CREATE A METHOD TO CHECK THE NETWORK CONNECTION
         *
         * */
-        PsiMethod psiCheckNetworkMethod = factory.createMethod("checkNetwork", PsiType.BOOLEAN);
+        String psiCheckNetworkMethodString =
+                "private boolean checkNetwork() {\n" +
+                "\t\tfinal ConnectivityManager connManager =\n" +
+                "\t\t\t(ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);\n" +
+                "\t\tfinal NetworkInfo net = connManager.getActiveNetworkInfo();\n" +
+                "\t\tfinal boolean isConnected = net != null && net.isConnected();\n" +
+                "\n" +
+                "\t\treturn isConnected;\n" +
+                "\t}";
+        PsiMethod psiCheckNetworkMethod = factory.createMethodFromText(psiCheckNetworkMethodString, null);
+        //TODO: adicionar à class referida em cima
 
         /*
         *
@@ -49,27 +67,61 @@ public class CheckNetworkQuickFix implements LocalQuickFix {
         *           2. enable
         *           3. disable
         * */
-        PsiClass broadcastReceiverClass = factory.createClass("NetworkStateReceiver");
-        // extends class
-        //broadcastReceiverClass.getExtendsList().add(android.content.BroadcastReceiver.class);
-
-        // onReceive Method
-        PsiMethod onReceiveMethod = factory.createMethod("onReceive", PsiType.VOID);
-        // TODO: como é que eu crio um parametro que nao é um tipo primitivo ?
-        // !!! a class PsiType tem um metodo estatico chamado getTypeByName que encontra a classe pelo qualified class name,
-        // no entanto é dentro de um certo scope !!!
-        //PsiParameter contextParameter = factory.createParameter("context",);
-        PsiCodeBlock onReceiveMethodBlock = onReceiveMethod.getBody();
-
-
-        // enable Method
-        PsiMethod enableMethod = factory.createMethod("enable", PsiType.VOID);
-        PsiCodeBlock enableMethodBody = onReceiveMethod.getBody();
-
-
-        // disable Method
-        PsiMethod disabelMethod = factory.createMethod("disable", PsiType.VOID);
-        PsiCodeBlock disableMethodBody = onReceiveMethod.getBody();
+        String broadcastReceiverString =
+                "public static class NetworkStateReceiver extends BroadcastReceiver {\n" +
+                "\t\tprivate static final String TAG = NetworkStateReceiver.class.getName();\n" +
+                "\n" +
+                "\t\t@Override\n" +
+                "\t\tpublic void onReceive(Context context, Intent intent) {\n" +
+                "\t\t\tandroid.util.Log.i(TAG, \"Network state change received.\");\n" +
+                "\n" +
+                "\t\t\tfinal AutoRefreshHelper helper =\n" +
+                "\t\t\t\tAutoRefreshHelper.getInstance(context.getApplicationContext());\n" +
+                "\n" +
+                "\t\t\tif (helper.checkNetwork()) {\n" +
+                "\t\t\t\thelper.rescheduleAlarm();\n" +
+                "\t\t\t}\n" +
+                "\t\t}\n" +
+                "\n" +
+                "\t\tpublic static void enable(Context context) {\n" +
+                "\t\t\tfinal android.content.pm.PackageManager packageManager = context.getPackageManager();\n" +
+                "\n" +
+                "\t\t\tfinal android.content.ComponentName receiver =\n" +
+                "\t\t\t\tnew ComponentName(context, NetworkStateReceiver.class);\n" +
+                "\n" +
+                "\t\t\tif (packageManager.getComponentEnabledSetting(receiver) !=\n" +
+                "\t\t\t    PackageManager.COMPONENT_ENABLED_STATE_ENABLED) {\n" +
+                "\t\t\t\tandroid.util.Log.i(TAG, \"Enabling network state receiver.\");\n" +
+                "\t\t\t}\n" +
+                "\n" +
+                "\t\t\tpackageManager.setComponentEnabledSetting(\n" +
+                "\t\t\t\treceiver,\n" +
+                "\t\t\t\tandroid.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED,\n" +
+                "\t\t\t\tandroid.content.pm.PackageManager.DONT_KILL_APP);\n" +
+                "\t\t}\n" +
+                "\n" +
+                "\t\tpublic static void disable(Context context) {\n" +
+                "\t\t\tfinal android.content.pm.PackageManager packageManager = context.getPackageManager();\n" +
+                "\n" +
+                "\t\t\tfinal android.content.ComponentName receiver =\n" +
+                "\t\t\t\tnew ComponentName(context, NetworkStateReceiver.class);\n" +
+                "\n" +
+                "\t\t\tif (packageManager.getComponentEnabledSetting(receiver) !=\n" +
+                "\t\t\t    android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED) {\n" +
+                "\t\t\t\tandroid.util.Log.i(TAG, \"Disabling network state receiver.\");\n" +
+                "\t\t\t}\n" +
+                "\n" +
+                "\t\t\tpackageManager.setComponentEnabledSetting(\n" +
+                "\t\t\t\treceiver,\n" +
+                "\t\t\t\tandroid.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED,\n" +
+                "\t\t\t\tandroid.content.pm.PackageManager.DONT_KILL_APP);\n" +
+                "\t\t}" +
+                "\n} ";
+        PsiClass broadcastReceiverClass = factory.createClassFromText(broadcastReceiverString, null);
+        intentServiceClass.addAfter(broadcastReceiverClass.getInnerClasses()[0], intentServiceClass.getRBrace());
+        // para fazer imports é assim! - tem que ser sempre com o fully qualified name na string e depois chamar o metodo shortenClassReferences !!
+        JavaCodeStyleManager javaCodeStyleManager = JavaCodeStyleManager.getInstance(project);
+        javaCodeStyleManager.shortenClassReferences(intentServiceClass);
 
 
 
